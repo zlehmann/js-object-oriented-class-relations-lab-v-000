b73cc7ca19707818f6ce1879eed5d19d3698346c
Zachary Lehmann <zlehmann@gmail.com>
2019-03-23 23:33:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,6 +42,10 @@ class Trip {
   }
 
   driver() {
-    return this.driver;
+    return store.drivers.find(
+      function(driver) {
+        return driver.id === this.driverId;
+      }.bind(this)
+    );
   }
 }
