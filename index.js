02379a23e0dd4370886c2f8f2fd4fed92f975be5
Zachary Lehmann <zlehmann@gmail.com>
2019-03-23 23:29:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,13 @@ class Trip {
     this.id = ++tripId;
     this.driverId = driver.id;
     this.passengerId = passenger.id;
-    this.passenger = passenger;
 
     store.trips.push(this);
   }
 
+  passenger() {
+    return this.passenger;
+  }
 
   driver() {
     return this.driver;
